fed3eef2fccd1cfd3f6fad7076de450b1ba2433b
sawan shakya <sawanshakya1995@gmail.com>
2026-01-29 16:23:49 +0530

    update commit ymt folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,6 +10,11 @@ import Cart from './views/Cart/Cart'
 import Thankyou from './views/Cart/Thankyou'
 
 function App() {
+
+  // useEffect(() => {
+  //   console.log('process.env', import.meta.env)
+  // }, [])
+
   return (
     <BrowserRouter>
       <Routes>
